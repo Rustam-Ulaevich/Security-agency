c706ff98bb578479d979fdeeab48609d11067b36
Rustam-Ulaevich <praporglamur@gmail.com>
2022-07-02 22:50:43 +0500

    create new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import scss from './index.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+          <div className='headerLeft'>
+              <img src='/image/scale_1200.webp' height={'20px'} />
+              <div className='headerLabel'>
+                  <h3>Guardians of the Galaxy</h3>
+                  <p>Security agency</p>
+              </div>
+          </div>
+          <ul className='headerRight'>
+              <li>
+                  <img src='/image/cart.png'/>
+                  <span>5600 $</span>
+              </li>
+              <li>
+                  <img src='/image/user.png' height={'20px'}/>
+              </li>
+          </ul>
       </header>
+        <div className='content'>
+            <h3>All the guards</h3>
+            ....
+        </div>
     </div>
   );
 }
