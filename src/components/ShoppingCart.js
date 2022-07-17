04887bbb13b344b9e3bf9e5c955fd3b3bace2694
Rustam-Ulaevich <praporglamur@gmail.com>
2022-07-17 21:09:39 +0500

    refactoring ShoppingCart.js

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -3,15 +3,35 @@ import {useContext, useState} from "react";
 import {AppContext} from "../context";
 import axios from "axios";
 
+const delay = (ms) => new Promise( (resolve) => setTimeout(resolve, ms))
+
 export function ShoppingCart({onClickCart, removeItemCart, items = []}) {
-    const {itemCart, setItemCart} = useContext(AppContext)
+    const {cartItems, setСartItems} = useContext(AppContext)
     const [orderId, setOrderId] = useState(null)
     const [order, setOrder] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
 
-    const onClickOrder = () => {
-        axios.post('https://62c3ffff7d83a75e39ecd122.mockapi.io/orders', itemCart);
-        setOrder(true)
-        setItemCart([])
+    const onClickOrder = async () => {
+        try {
+            setIsLoading(true)
+            const {data} = await axios.post('https://62c3ffff7d83a75e39ecd122.mockapi.io/orders', {
+                items: cartItems
+            });
+            //await axios.put('https://62c3ffff7d83a75e39ecd122.mockapi.io/shoppingCart', [])
+            setOrderId(data.id)
+            setOrder(true)
+            setСartItems([])
+
+            for (let i=0; i<cartItems.length; i++){
+                const item = cartItems[i]
+                await axios.delete('https://62c3ffff7d83a75e39ecd122.mockapi.io/shoppingCart/' + item.id );
+                await delay(1000)
+            }
+
+        } catch (error){
+            alert('Failed to create an order :(')
+        }
+        setIsLoading(false)
     }
 
     return (
@@ -20,7 +40,6 @@ export function ShoppingCart({onClickCart, removeItemCart, items = []}) {
                 <h2 className='d-flex justify-between mb-20'>Cart
                     <img onClick={onClickCart} className='removeBtn cu-p' src='/image/removeClick.svg' alt='Remove'/>
                 </h2>
-
                 {
                     items.length > 0 ?
                         <div className='d-flex flex-column flex'>
@@ -52,14 +71,14 @@ export function ShoppingCart({onClickCart, removeItemCart, items = []}) {
                                         <b>45,5 руб</b>
                                     </li>
                                 </ul>
-                                <button onClick={onClickOrder} className='greenButton'>Place an order<img
+                                <button disabled={isLoading} onClick={onClickOrder} className='greenButton'>Place an order<img
                                     src='/image/strelka.svg' alt='Arrow'/>
                                 </button>
                             </div>
                         </div>
                         : (
                             <Info title = { order ? 'The order has been placed!' : 'The basket is empty'}
-                                  description = { order ? 'Your order #18 will be delivered to courier delivery soon' : 'Add at least one security guard to place an order'}
+                                  description = { order ? `Your order #${orderId} will be delivered to courier delivery soon` : 'Add at least one security guard to place an order'}
                                   image = {order ? '/image/order.svg' : '/image/cartEmpty.svg'}
                             />
                         )}
